d2150a17ee10b38c49f9bda89374c0ed474e69cc
astarove <anatoliy.staroverov1234@gmail.com>
2020-12-09 19:55:41 -0800

    added tests for loop check and modified loop check.

--- src/main/java/eclipse_plugin/NumberOfLoopsCheck.java
+++ src/main/java/eclipse_plugin/NumberOfLoopsCheck.java
@@ -8,8 +8,15 @@ public class NumberOfLoopsCheck extends AbstractCheck {
 	public int loopMax = 0;
 	public int loopCount;
 	
+	public int for_count;
+	public int do_count;
+	public int while_count;
+	
 	public void beginTree(DetailAST rootAST) {
 		loopCount = 0;
+		while_count = 0;
+		do_count = 0;
+		for_count = 0;
 	}
 	
 	@Override
@@ -24,12 +31,20 @@ public class NumberOfLoopsCheck extends AbstractCheck {
 
 	@Override
 	public int[] getRequiredTokens() {
-		return new int[] { TokenTypes.LITERAL_FOR, TokenTypes.LITERAL_WHILE, TokenTypes.LITERAL_DO, };
+		return new int[] { 
+				TokenTypes.LITERAL_FOR, 
+				TokenTypes.LITERAL_WHILE, 
+				TokenTypes.LITERAL_DO, 
+				};
 	}
 
 	@Override
 	public int[] getDefaultTokens() {
-		return new int[] { TokenTypes.LITERAL_FOR, TokenTypes.LITERAL_WHILE, TokenTypes.LITERAL_DO };
+		return new int[] {
+				TokenTypes.LITERAL_FOR, 
+				TokenTypes.LITERAL_WHILE, 
+				TokenTypes.LITERAL_DO 
+				};
 	}
 
 	public void setMax(int max) {
@@ -42,14 +57,17 @@ public class NumberOfLoopsCheck extends AbstractCheck {
 		switch (ast.getType()) {
 		case TokenTypes.LITERAL_FOR:
 			loopCount++;
+			for_count++;
 			break;
 
 		case TokenTypes.LITERAL_WHILE:
 			loopCount++;
+			while_count++;
 			break;
 
 		case TokenTypes.LITERAL_DO:
 			loopCount++;
+			do_count++;
 			break;
 		}
 	}
