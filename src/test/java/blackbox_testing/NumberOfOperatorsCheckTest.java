b83216147cad37d350fc5c0daa82cc195d2de074
astarove <anatoliy.staroverov1234@gmail.com>
2020-12-10 00:58:51 -0800

    started blackbox test for Number of Operands.

---
+++ src/test/java/blackbox_testing/NumberOfOperatorsCheckTest.java
@@ -0,0 +1,15 @@
+package blackbox_testing;
+import eclipse_plugin.NumberOfOperatorCheck;
+
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+
+public class NumberOfOperatorsCheckTest {
+	
+	
+}
